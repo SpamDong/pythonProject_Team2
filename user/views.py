2e2d661c64b557d3093b524fff68d6624efca4a5
Hongin-Lim <hongsec90@gmail.com>
2022-01-18 22:39:52 +0900

    우선 views.py에 login, logout 함수만 작성 함수내 HTML경로는 재수정 해야함

--- user/views.py
+++ user/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+def signup(reqeust):
+    if reqeust.method == "GET":
+        signupForm = UserCreationForm()
+        return render(reqeust, 'user/signup.html',{'signupForm':signupForm})
+    elif reqeust.method == "POST":
+        signupForm = userCreationForm(request.POST)
+        if signupForm.is_valid():
+            signupForm.save()
+            return redirect('/user/signup')
+
+def userlogin(request):
+    if request.method == "GET":
+        loginForm = AuthenticationForm()
+        return render(request, 'user/login.html', {'loginForm':loginForm})
+    elif request.method == "POST":
+        loginForm = AuthenticationForm(request, request.POST)
+        if loginForm.is_valid():
+            login(request, loginForm.get_user())
+            return  redirect('/post/post')
+        else:
+            retuen redirect('/post/post')
+
+def userlogout(request):
+    loout(request)
+    return  redirect('user/login')
