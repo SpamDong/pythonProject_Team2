456ed22147e0a00dfcdcd123322a6c469bbacb55
Hongin-Lim <hongsec90@gmail.com>
2022-01-18 23:20:58 +0900

    user/views.py 로그인, 로그아웃 관련 함수 작성중

--- user/views.py
+++ user/views.py
@@ -1,4 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth import logout, login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
-테스트ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+def signup(request):
+    if request.method == "GET":
+        signupForm = UserCreationForm()
+        return render(request, 'user/signup.html',{'signupForm':signupForm})
+    elif request.method == "POST":
+        signupForm = UserCreationForm(request.POST)
+        if signupForm.is_valid():
+            signupForm.save()
+            return redirect('/user/signup')
+
+def userlogin(request):
+    if request.method == "GET":
+        loginForm = AuthenticationForm()
+        return render(request, 'user/login.html', {'loginForm':loginForm})
+    elif request.method == "POST":
+        loginForm = AuthenticationForm(request, request.POST)
+        if loginForm.is_valid():
+            login(request, loginForm.get_user())
+            return  redirect('/post/post')
+        else:
+            return redirect('/post/post')
+
+def userlogout(request):
+    logout(request)
+    return  redirect('user/login')
+
