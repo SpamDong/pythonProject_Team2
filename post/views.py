ffb9c2d50b243360a74ecd3796b417ff99113d75
SpamDong <dksgusehd10@gmail.com>
2022-01-19 11:42:42 +0900

    comment 게시글 나눴습니다.

--- post/views.py
+++ post/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def post(request):
-    return render(request, 'post/post.html')
+    return render(request, 'main_post/main_post.html')
 
