fe18a6e19c60ce095508aa164057c85b6073682a
slinfo <zxcv8596@gmail.com>
2022-01-20 11:34:29 +0900

    html에서 줄 정리를 했고 if와 for를 사용할 예정

--- comment/models.py
+++ comment/models.py
@@ -2,7 +2,10 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+
 class Comment(models.Model):
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(hplace, related_name=hplace, on_delete=models.CASCADE)
     contents = models.TextField(max_length=200)
     create_date = models.DateTimeField(auto_now_add=True)
+
