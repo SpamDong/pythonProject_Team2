91bcbd7a06adfe9e34b7ba8b2e39dfeba2b46891
SpamDong <dksgusehd10@gmail.com>
2022-01-21 14:31:58 +0900

    commitㅇ

--- user/views.py
+++ user/views.py
@@ -1,6 +1,8 @@
-
-from django.contrib.auth import logout, login, authenticate
+from django.contrib import auth
+from django.contrib.auth import logout, login, authenticate, update_session_auth_hash
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
 
 from django.shortcuts import render, redirect
 from django.template.loader import render_to_string
